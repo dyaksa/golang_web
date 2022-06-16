0bb3b7be72e994d11ae2be2a4d67922b2f3f7eae
dyaksaa <diasnour0395@gmail.com>
2022-06-16 11:02:14 +0700

    serve mux

---
+++ handler_test.go
@@ -0,0 +1,23 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ servemux_test.go
@@ -0,0 +1,29 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello world")
+	})
+
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi!")
+	})
+
+	serve := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := serve.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
