11b2c9aecc1a27d424565462739efb50b96934d9
dyaksaa <diasnour0395@gmail.com>
2022-06-28 08:56:27 +0700

    redirect

---
+++ redirect_test.go
@@ -0,0 +1,31 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func RedirectTo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello Redirect")
+}
+
+func RedirectFrom(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/redirect-to", http.StatusTemporaryRedirect)
+}
+
+func TestServerRedirect(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/redirect-from", RedirectFrom)
+	mux.HandleFunc("/redirect-to", RedirectTo)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
